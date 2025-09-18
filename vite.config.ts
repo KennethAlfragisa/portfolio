3b6fe253e8a5cc752924f79eda725a311e45efb3
HubertusKenneth <hubertusk26@gmail.com>
2025-09-19 00:49:54 +0700

    push porto

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofollio/', 
+  base: '/portofolio/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
